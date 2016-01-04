b954cb98f5c1c50a912371b6e534603dd43f33e7
Chris Law <megs.law@gmail.com>
2016-01-04 20:56:33 +0000

    Adding more submodules

--- setupvim.sh
+++ setupvim.sh
@@ -6,7 +6,7 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim/bundle && \
 git clone https://github.com/scrooloose/syntastic.git
-
+git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
+git submodule add -f git://github.com/rodjek/vim-puppet.git .vim/bundle/puppet
 #pip install pylint
 
-git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
