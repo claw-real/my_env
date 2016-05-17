c2cde771a8f3b41a3750cbc5f8f62c00aa5762f2
Chris Law <megs.law@gmail.com>
2016-05-17 11:10:20 +0100

    Adding whitespace vim plugin

--- setupvim.sh
+++ setupvim.sh
@@ -7,7 +7,8 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle && \
 git clone https://github.com/scrooloose/syntastic.git
 git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
-git submodule add -f git://github.com/rodjek/vim-puppet.git .vim/bundle/puppet
+#git submodule add -f git://github.com/rodjek/vim-puppet.git .vim/bundle/puppet
+git clone git://github.com/ntpeters/vim-better-whitespace.git ~/.vim/bundle/vim-better-whitespace
 git clone https://github.com/rodjek/vim-puppet.git
 git clone https://github.com/godlygeek/tabular.git
 #pip install pylint
