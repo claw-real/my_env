e2dc9dfba6e8f98e4bdd3960a5bd922b6b202aba
Chris Law <chris.law@mediakind.com>
2021-03-18 10:46:37 +0000

    k8s setup

--- setup.sh
+++ setup.sh
@@ -19,3 +19,10 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tmux-yank ~/.tmux/plugins/tmux-yank
 cp .tmux.conf.local ~
 cp .tmux.conf ~
+
+## More prompts
+mkdir -p ~/git/others
+cd ~/git/others || exit
+git clone https://github.com/jonmosco/kube-ps1.git
+cd - || exit
+
