37c3a328472cae4689f88f14e4211b145cf218bc
Chris Law <chris.law@mediakind.com>
2021-07-08 13:30:05 +0100

    Undo kube_prompt

--- setup.sh
+++ setup.sh
@@ -20,9 +20,3 @@ git clone https://github.com/tmux-plugins/tmux-yank ~/.tmux/plugins/tmux-yank
 cp .tmux.conf.local ~
 cp .tmux.conf ~
 
-## More prompts
-mkdir -p ~/git/others
-cd ~/git/others || exit
-git clone https://github.com/jonmosco/kube-ps1.git
-cd - || exit
-
